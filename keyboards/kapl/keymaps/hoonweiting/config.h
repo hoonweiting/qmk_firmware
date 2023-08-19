31f1705763e792c1d9d3673f7a7fc7ccf406c58d
Wei Ting <hoonweiting@gmail.com>
2023-08-20 02:22:48 +0800

    Add encoder configuration for Kapl

--- keyboards/kapl/keymaps/hoonweiting/config.h
+++ keyboards/kapl/keymaps/hoonweiting/config.h
@@ -1,3 +1,13 @@
 // Copyright 2022 Alexander Lozyuk (@keyzog)
 // SPDX-License-Identifier: GPL-2.0-or-later
 #pragma once
+
+// Encoder support
+// Left half
+#define ENCODERS_PAD_A { D0 }
+#define ENCODERS_PAD_B { D1 }
+#define ENCODER_RESOLUTIONS { 2 }
+// Right half
+#define ENCODERS_PAD_A_RIGHT { D1 }
+#define ENCODERS_PAD_B_RIGHT { D0 }
+#define ENCODER_RESOLUTIONS_RIGHT { 2 }
