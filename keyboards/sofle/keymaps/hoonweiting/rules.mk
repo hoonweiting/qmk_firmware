eaeba3806a90e06a6415627b2330e7ec4af4d6f1
Wei Ting <hoonweiting@gmail.com>
2025-12-14 22:22:03 +0800

    Create custom Sofle keymap

---
+++ keyboards/sofle/keymaps/hoonweiting/rules.mk
@@ -0,0 +1,13 @@
+# Enable mouse keys
+MOUSEKEY_ENABLE = yes
+# Enable audio control and system control
+EXTRAKEY_ENABLE = yes
+# Enable encoders
+ENCODER_ENABLE = yes
+ENCODER_MAP_ENABLE = yes
+# Link Time Optimization
+LTO_ENABLE = yes
+# Sofle uses atmega32u4 by default
+CONVERT_TO = rp2040_ce
+# Enable configuration through VIA
+VIA_ENABLE = yes
